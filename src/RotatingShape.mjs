ed22405c8f88c69f3890bd215ac5dac455a9487a
piaand <pia.m.andersin@gmail.com>
2021-12-17 19:09:57 +0200

    added rotate left

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -35,6 +35,30 @@ export class RotatingShape {
         shapeString[i] +
         newShapeString.substr(newPosition + 1);
     }
+    this.flatShape = newShapeString;
+    return this;
+  }
+
+  rotateLeft() {
+    const shapeString = this.flatShape;
+    const shapeLength = shapeString.length;
+    let newShapeString = new Array(shapeLength + 1).join(".");
+
+    for (var i = 0; i < shapeLength; i++) {
+      //convert to x/y
+      var x = i % this.side;
+      var y = Math.floor(i / this.side);
+
+      //find new x/y
+      var newY = this.side - x - 1;
+      var newX = y;
+
+      var newPosition = newY * this.side + newX;
+      newShapeString =
+        newShapeString.substr(0, newPosition) +
+        shapeString[i] +
+        newShapeString.substr(newPosition + 1);
+    }
     console.log(newShapeString);
     this.flatShape = newShapeString;
     return this;
